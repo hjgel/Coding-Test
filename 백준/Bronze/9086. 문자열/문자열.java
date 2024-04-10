1f5f3c71598d512d1236d698162ccae3a82ba885
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-04-10 21:44:45 +0900

    [Bronze V] Title: 문자열, Time: 124 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+
+
+		for(int i = 0; i < N; i++){
+			String S = br.readLine();
+			StringBuilder sb = new StringBuilder();
+			sb.append(S.charAt(0)).append(S.charAt(S.length()-1));
+			System.out.println(sb);
+		}
+	}
+}
