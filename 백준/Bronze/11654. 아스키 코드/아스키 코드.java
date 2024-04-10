80344b8110e52dd6bb1d3e942386c5f25110a50e
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-04-10 21:48:06 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char N = br.readLine().charAt(0);
+
+		System.out.println((int) N);
+
+		
+	}
+}
