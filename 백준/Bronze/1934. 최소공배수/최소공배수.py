71a0d8064b32b05b5f00f6196c3aa6513c6f68f6
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-04-11 10:01:36 +0900

    [Bronze I] Title: 최소공배수, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,14 @@
+def gcd(a, b):
+    if(b == 0):
+        return a
+    return gcd(b, a%b)
+
+def shortGcd(a,b):
+    return a * b // gcd(a, b)
+
+N = int(input())
+
+for i in range(N):
+    a, b = map(int, input().split())
+    print(int(shortGcd(a,b)))
+    
