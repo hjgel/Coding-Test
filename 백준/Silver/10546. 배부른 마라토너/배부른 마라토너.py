13d957a9a4d70d95eeecd38e2fbf5bce46f74582
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-05-04 20:01:36 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 336 ms, Memory: 150416 KB -BaekjoonHub

---
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.py
@@ -0,0 +1,20 @@
+from collections import defaultdict, Counter
+def solution(participant, completion):
+    answer = ''
+    sH = Counter(participant)
+    for x in completion:
+        sH[x] -= 1
+    for key in sH:
+        if sH[key] == 1:
+            return key
+    return answer
+
+
+n = int(input())
+a = []
+for i in range(n):
+    a.append(input())
+b = []
+for i in range(n-1):
+    b.append(input())
+print(solution(a, b))
