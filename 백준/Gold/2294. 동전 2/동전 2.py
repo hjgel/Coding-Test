5748f2e45c1f4f3ce7ffb318de70a4d9fe06147f
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-05-21 16:46:43 +0900

    [Gold V] Title: 동전 2, Time: 356 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,6 +1,6 @@
 a, b = map(int, input().split())
 
-dp = [10001] * (b+1)
+dp = [b+1] * (b+1)
 dp[0] = 0
 li = []
 for _ in range(a): li.append(int(input()))
@@ -11,5 +11,5 @@ for i in li:
     for idx in range(i, b+1):
         if dp[idx] > 0: dp[idx] = min(dp[idx], dp[idx-i] + 1)
 
-if dp[b] == 10001: print(-1)
+if dp[b] == b+1: print(-1)
 else: print(dp[b])
