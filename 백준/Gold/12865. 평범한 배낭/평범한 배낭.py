a9a0775a6cfcbe85be18da9a31b7a1439bccf067
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-05-27 16:20:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 6260 ms, Memory: 229084 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+dp = [[0] * (K+1) for _ in range(N+1)]
+bag = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, N+1):
+    for j in range(1, K+1):
+        if j >= bag[i-1][0]:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-bag[i-1][0]] + bag[i-1][1])
+        else:
+            dp[i][j] = dp[i-1][j]
+print(dp[N][K])
