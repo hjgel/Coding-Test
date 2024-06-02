06a2566933dd30a4b9664bac8022a658217172ad
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-06-02 14:06:53 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 480 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.py
@@ -0,0 +1,11 @@
+li = list(map(int,input().split()))
+n = 1
+while True:
+    cnt = 0
+    for i in li:
+        if n%i==0:
+            cnt+=1
+    if cnt>=3:
+        print(n)
+        break
+    n+=1
