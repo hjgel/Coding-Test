020600454e444bf32f61ed4a2daea2c6a8a52b3f
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2024-09-29 09:52:17 +0900

    [Gold V] Title: LCS, Time: 488 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,11 @@
+A = input()
+B = input()
+
+LCS = [[0] * (len(B) +1) for _ in range(len(A) + 1)]
+
+for i in range(1, len(A) + 1):
+    for j in range(1, len(B) + 1):
+        if A[i-1] == B[j-1]: LCS[i][j] = LCS[i - 1][j - 1] + 1
+        else: LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+
+print(max(map(max,LCS)))
