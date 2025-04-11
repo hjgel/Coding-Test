514d2a71aae79fa41600e5f5223403e1b359a544
Hyun-Jun Lee <143315340+hjgel@users.noreply.github.com>
2025-04-11 17:51:24 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args){
-    	 Scanner sc = new Scanner(System.in);
-    	 int X = sc.nextInt();
-         int N = sc.nextInt();
-         int result = 0;
-         
-         for(int i = 0; i < N; i++) {
-        	 int a = sc.nextInt();
-        	 int b = sc.nextInt();
-        	 result = result + (a*b);
-         }
-         if (X == result) System.out.println("Yes");
-         else System.out.println("No");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(reader.readLine());
+        int N = Integer.parseInt(reader.readLine());
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(reader.readLine()); // 줄바꿈 전에 공백으로 분할해줌
+            int a = Integer.parseInt(st.nextToken()); // 물건 가격
+            int b = Integer.parseInt(st.nextToken()); // 물건 개수
+            result += (a * b);
+        }
+
+        if (result == X) { System.out.println("Yes"); }
+        else { System.out.println("No"); }
     }
 }
